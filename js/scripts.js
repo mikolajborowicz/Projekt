ef6b973508cfea69f60f548adeab83afa4499d27
Your Name <mikolaj.borowicz@szkola.salezjanie.com>
2018-07-17 22:47:31 +0200

    commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var myName;
+myName = "Mikolaj"
+
+var date = "17.07.2018"
+console.log("may name is Mikolaj")
+
+
+var num = 12
+console.log(++num)
+
+var number
+
+console.log(12/2)
+
+console.log(num+=5)
+
+const newLocal = "Mikołaj";
+var myName = newLocal
+
+function squere(number) {
+    return number * number;  
+}
+
+console.log(squere(number))
--- notatki.md
+++ notatki.md
@@ -1,3 +1,26 @@
+
+EDYCJA TEKSTU
+ 
 <p> akapit
 <em> pochylenie
 <be> przerzutnia
+
+
+js script- 
+java script to język, który pomaga w chwytaniu części strony, stosuje funkcje i definiuje elementy strony.
+
+    Zmienne 
+    
+    var myName = "wartośćZmiennej";
+
+
+
+
+
+
+
+
+
+
+
+
